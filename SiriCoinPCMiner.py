aba0764fe0d3dd2dad4e7e03495fb08227e17ccf
Gustas Savickas <75716744+the-red-eye-studio@users.noreply.github.com>
2022-07-14 22:17:29 +0300

    Update to v3

--- SiriCoinPCMiner.py
+++ SiriCoinPCMiner.py
@@ -1,27 +1,42 @@
-import time, json, sha3, sys, os, termcolor, requests, cpuinfo, platform, psutil
-from web3.auto import w3
+import time, json, sha3, sys, os, requests, cpuinfo, platform, psutil
 from eth_account.messages import encode_defunct
+from web3.auto import w3
+from rich import print
 
 
 MainNET = "https://node-1.siricoin.tech:5006"
-ShreyasNET = "http://138.197.181.206:5005"
 JunaidNET = 'http://47.250.59.81:5005'
 
+configFile = "config.json"
 TimeOUT = 1
+hashes_per_list = 3000
+hashrate_refreshRate = 15 # s
 
-if os.name == 'nt': os.system('color')
 
+
+def rgbPrint(string, color, end="\n"):
+    print("[" + color + "]" + str(string) + "[/" + color + "]", end=end)
 
 def Get_address():
     global address_valid, minerAddr
+
+    if os.path.exists(configFile):
+        address_valid = True
+        with open(configFile) as f:
+            data = json.load(f)
+        minerAddr = data["address"]
+
     while not address_valid:
-        minerAddr = input(termcolor.colored("Enter your SiriCoin address: ", 'magenta'))
-        try:
-            address_valid = w3.isAddress(minerAddr)
-        except:
-            print(termcolor.colored("The address you inputed is invalid, please try again", "red"))
-        if not address_valid:
-            print(termcolor.colored("The address you inputed is invalid, please try again", "red"))
+            minerAddr = input("Enter your SiriCoin address: ")
+            try:
+                address_valid = w3.isAddress(minerAddr)
+            except:
+                rgbPrint("The address you inputed is invalid, please try again", "red")
+            if not address_valid:
+                rgbPrint("The address you inputed is invalid, please try again", "red")
+            else:
+                with open(configFile, 'w') as outfile:
+                    json.dump({"address": minerAddr}, outfile)
     
 
 class SignatureManager(object):
@@ -45,7 +60,7 @@ class SiriCoinMiner(object):
         self.node = NodeAddr
         self.signer = SignatureManager()
         self.difficulty = 1
-        self.target = "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+        self.target = "0x" + "f"*64
         self.lastBlock = ""
         self.rewardsRecipient = w3.toChecksumAddress(RewardsRecipient)
         self.priv_key = w3.solidityKeccak(["string", "address"], ["SiriCoin Will go to M00N - Just a disposable key", self.rewardsRecipient])
@@ -74,18 +89,12 @@ class SiriCoinMiner(object):
         self.refresh()
         try:
             txid = requests.get(f"{self.node}/send/rawtransaction/?tx={json.dumps(tx).encode().hex()}").json().get("result")[0]
-            print(termcolor.colored(f"Mined block {blockData['miningData']['proof']},\nsubmitted in transaction {txid}", "green"))
-            print(termcolor.colored("Current Network Balance: " + str(requests.get(f"{self.node}/accounts/accountBalance/{self.rewardsRecipient}").json()["result"]["balance"]) + " Siri", "green")) # code by luketherock868
+            rgbPrint(f"Mined block {blockData['miningData']['proof']},\nsubmitted in transaction {txid}", "green")
+            rgbPrint("Current Network Balance: " + str(requests.get(f"{self.node}/accounts/accountBalance/{self.rewardsRecipient}").json()["result"]["balance"]) + " Siri", "green") # code by luketherock868
             miner.startMining()
         except:
-            print(termcolor.colored("Oops, failed subminting the block, wait till mainnet is back up and enter in this URL in your browser, ignore the output, just do it ;D \n", "red"))
-            print(termcolor.colored(str(f"https://siricoin-node-1.dynamic-dns.net:5005/send/rawtransaction/?tx={json.dumps(tx).encode().hex()}", "cyan")))
-            sys.exit()
+            pass
             
-
-
-
-
     def beaconRoot(self):
         messagesHash = sha3.keccak_256(self.messages).digest()
         bRoot = "0x" + sha3.keccak_256((b"".join([bytes.fromhex(self.lastBlock.replace("0x", "")), int(self.timestamp).to_bytes(32, 'big'), messagesHash, bytes.fromhex(self.rewardsRecipient.replace("0x", "")) ]))).hexdigest() # parent PoW hash (bytes32), beacon's timestamp (uint256), hash of messages (bytes32), beacon miner (address)
@@ -98,39 +107,52 @@ class SiriCoinMiner(object):
     def formatHashrate(self, hashrate):
         if hashrate < 1000:
             # H's
-            if len(str(round(hashrate, 2)).split('.', 1)[1]) == 1: return str(round(hashrate/1000, 2)) + "0 H/s" 
-            else: return str(round(hashrate/1000, 2)) + " H/s"
+            if len(str(round(hashrate, 2)).split('.', 1)[1]) == 1: return str(round(hashrate, 2)) + "0 H/s" 
+            else: return str(round(hashrate, 2)) + " H/s"
         elif hashrate < 1000000:
             # KH's
             if len(str(round(hashrate/1000, 2)).split('.', 1)[1]) == 1: return str(round(hashrate/1000, 2)) + "0 KH/s" 
             else: return str(round(hashrate/1000, 2)) + " KH/s"
         elif hashrate < 1000000000:
             # MH's
-            if len(str(round(hashrate/1000000, 2)).split('.', 1)[1]) == 1: return str(round(hashrate/1000, 2)) + "0 MH/s" 
-            else: return str(round(hashrate/1000, 2)) + " MH/s"
+            if len(str(round(hashrate/1000000, 2)).split('.', 1)[1]) == 1: return str(round(hashrate/1000000, 2)) + "0 MH/s" 
+            else: return str(round(hashrate/1000000, 2)) + " MH/s"
         elif hashrate < 1000000000000:
             # GH's
-            if len(str(round(hashrate/1000000000, 2)).split('.', 1)[1]) == 1: return str(round(hashrate/1000, 2)) + "0 GH/s" 
-            else: return str(round(hashrate/1000, 2)) + " GH/s"
+            if len(str(round(hashrate/1000000000, 2)).split('.', 1)[1]) == 1: return str(round(hashrate/10000000, 2)) + "0 GH/s" 
+            else: return str(round(hashrate/10000000, 2)) + " GH/s"
         
-            
-            
+                
     def startMining(self):
         self.refresh()
         global first_run, bRoot
-        if first_run: print(termcolor.colored(f"Started mining for {self.rewardsRecipient} on {the_node}", "yellow"))
-        proof = "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+        if first_run: rgbPrint(f"Started mining for {self.rewardsRecipient} on {the_node}", "yellow")
+        hashes = []
+
         while True:
             self.refresh()
             bRoot = self.beaconRoot()
+            bRoot_hasher = sha3.keccak_256()
+            bRoot_hasher.update(bytes.fromhex(bRoot.replace("0x", "")))
+
             first_run = False
-            while (time.time() - self.timestamp) < 30:
-                self.nonce += 1
-                proof = self.proofOfWork(bRoot, self.nonce)
-                if (int(proof, 16) < int(self.target, 16)):
-                    rawTX = ({"miningData" : {"miner": self.rewardsRecipient,"nonce": self.nonce,"difficulty": self.difficulty,"miningTarget": self.target,"proof": proof}, "parent": self.lastBlock,"messages": self.messages.hex(), "timestamp": int(self.timestamp), "son": "0000000000000000000000000000000000000000000000000000000000000000"})
-                    self.submitBlock(rawTX)
-            print(termcolor.colored("Last 30 seconds hashrate: " + self.formatHashrate((self.nonce / 30)), "yellow"))
+            while (time.time() - self.timestamp) < hashrate_refreshRate:
+
+                for i in range (hashes_per_list):
+                    finalHash = bRoot_hasher.copy()
+                    finalHash.update(self.nonce.to_bytes(32, "big"))
+                    hashes.append(finalHash)
+                    self.nonce +=1
+
+                for hash in hashes:
+                    if int(int.from_bytes(hash.digest(), "big")) < int(self.target, 16):
+                        validNonce = (self.nonce - (len(hashes) - hashes.index(hash)))
+                        rawTX = ({"miningData" : {"miner": self.rewardsRecipient, "nonce": validNonce, "difficulty": self.difficulty, "miningTarget": self.target, "proof": "0x" + hash.hexdigest()}, "parent": self.lastBlock, "messages": self.messages.hex(), "timestamp": int(self.timestamp), "son": "0"*64})           
+                        self.submitBlock(rawTX)
+
+                hashes.clear()
+
+            rgbPrint("Last " + str(hashrate_refreshRate) + "s hashrate: " + self.formatHashrate((self.nonce / hashrate_refreshRate)), "yellow")
 
 
 if __name__ == "__main__":
@@ -138,11 +160,11 @@ if __name__ == "__main__":
     address_valid = False
     
     
-    print(termcolor.colored("---------------------------- System ----------------------------", "yellow"))
-    print(termcolor.colored("OS: " + platform.system() + " " + platform.release(), "yellow"))
-    print(termcolor.colored("CPU: " + cpuinfo.get_cpu_info()['brand_raw'] +" @ "+ cpuinfo.get_cpu_info()['hz_advertised_friendly'] + " (x"+str(cpuinfo.get_cpu_info()['count'])+")", "yellow"))
-    print(termcolor.colored("RAM: " + str(round(int(psutil.virtual_memory()[3]) / 1074000000, 2)) + " / " + str(round(float(psutil.virtual_memory()[0]) / 1074000000, 2)) + " GB", "yellow"))
-    print(termcolor.colored("----------------------------------------------------------------", "yellow")) # code by luketherock868
+    rgbPrint("-"*28 + " System " + "-"*28, "yellow")
+    rgbPrint("OS: " + platform.system() + " " + platform.release(), "yellow")
+    rgbPrint("CPU: " + cpuinfo.get_cpu_info()['brand_raw'] +" @ "+ cpuinfo.get_cpu_info()['hz_advertised_friendly'] + " (x"+str(cpuinfo.get_cpu_info()['count'])+")", "yellow")
+    rgbPrint("RAM: " + str(round(int(psutil.virtual_memory()[3]) / 1074000000, 2)) + " / " + str(round(float(psutil.virtual_memory()[0]) / 1074000000, 2)) + " GB", "yellow")
+    rgbPrint("-"*64, "yellow") # code by luketherock868
     
     Get_address()
         
@@ -150,35 +172,16 @@ if __name__ == "__main__":
         if requests.get(f"{MainNET}/chain/block/2", timeout=TimeOUT).json()["result"]["height"] == 2:
             the_node = "main-net"
             miner = SiriCoinMiner(MainNET, minerAddr)
-            Continue_To_Shreyas_net = False
+            Continue_To_Junaid_net = False
             miner.startMining()
     except requests.ConnectTimeout:
-        Continue_To_Shreyas_net = True
-    
-    try:
-        if Continue_To_Shreyas_net and requests.get(f"{ShreyasNET}/chain/block/1", timeout=TimeOUT).json()["result"]["height"] == 1:
-            Continue_To_Junaid_net = False
-            print(termcolor.colored("Whoops! The main-net is offline, you can try mining on the Shreyas-net, but it's not recommended as it's not designed to mine, if you hit a block the balance won't move over to main-net.", "red"))
-            on_ShreyasNET = input(termcolor.colored("Are you sure you wan't to continue? Y/n \n", "magenta"))
-            if on_ShreyasNET.lower() == "y":
-                the_node = "Shreyas-net"
-                miner = SiriCoinMiner(ShreyasNET, minerAddr)
-                miner.startMining()
-            if on_ShreyasNET.lower() == "n":
-                print(termcolor.colored("Ok, quitting the miner, goodbye!", "red"))
-                sys.exit()
-            
-    except requests.ConnectTimeout:
-
         Continue_To_Junaid_net = True
 
     try:
         if Continue_To_Junaid_net and requests.get(f"{JunaidNET}/chain/block/1", timeout=TimeOUT).json()["result"]["height"] == 1:
             the_node = "Junaid-net"
             miner = SiriCoinMiner(JunaidNET, minerAddr)
-            Continue_To_main_net = False
             miner.startMining()
     except requests.ConnectTimeout:
-        print(termcolor.colored("All networks are down, quitting the miner, goodbye!", "red"))
+        rgbPrint("All networks are down, quitting the miner, goodbye!", "red")
         sys.exit()
-        
